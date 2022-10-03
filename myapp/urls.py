a1c99ba2d766f8b4779d1d69aa76e05d41b69109
theoverse <theoverse@protonmail.com>
2022-10-03 08:15:45 -0700

    using namespacing urls in myapp

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'myapp'
 urlpatterns = [
     path('', views.index),
     path('products/',views.products),
