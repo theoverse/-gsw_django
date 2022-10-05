82d22c98d5e08e1b83f0382ee21053179391169f
theoverse <theoverse@protonmail.com>
2022-10-05 10:49:26 -0700

    added image field to myapp model, and image to myapp index template

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,4 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     desc = models.CharField(max_length=200)
+    image = models.ImageField(blank=True,upload_to='images')
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import include,path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/',include('myapp.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
