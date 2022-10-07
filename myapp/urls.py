f975b0d78d910c710703b5f556ac7d9e4e8b8c15
theoverse <theoverse@protonmail.com>
2022-10-07 10:16:01 -0700

    created form 'addproduct', 'add_product' view and url path add to myapp

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 app_name = 'myapp'
+
 urlpatterns = [
     path('', views.index),
     path('products/',views.products),
     path('products/<int:id>/',views.product_detail,name='product_detail'),
+    path('products/add/',views.add_product,name='add_product'),
 ]
